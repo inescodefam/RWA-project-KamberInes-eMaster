363e8e30b347386a80b4638c9d6ec5c60ef3ae77
inescodefam <ines.codefam@gmail.com>
2025-07-07 13:29:51 +0200

    Fix view models

--- WebApp/Models/AuthVM.cs
+++ WebApp/Models/AuthVM.cs
@@ -6,12 +6,12 @@ namespace WebApp.Models
     {
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address format.")]
-        [Display(Name = "Email adresa")]
+        [Display(Name = "Email address")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Lozinka mora imati najmanje 6 znakova.")]
-        [Display(Name = "Lozinka")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be 8-100 characters.")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         public string? ConfirmPassword { get; set; }
